6a0dce8a25838d357193ba50b2e2327a3c4537d3
Ramarduk <ramarduk@gmail.com>
2019-07-11 14:48:52 -0600

    changes from terrence

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+/*To complete this challenge requires:
+
+Using DOM manipulation and traversal to dynamically add and remove HTML elements and apply styles.
+Linking to an externally hosted library (jQuery) and a locally hosted JavaScript file (index.js).
+Linking to your application JavaScript file from the index.html page.
+Using this and event delegation
+Requirements
+In terms of user experience, your shopping list app must allow users to:
+
+1. enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button.
+2. check and uncheck items on the list by clicking the "Check" button.
+3. permanently remove items from the list*/
+
+function addItem(),
